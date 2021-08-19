dd3de466b8bbb6081e4844072321edafaeb7affb
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-08-19 18:19:34 +0300

    add cors, router and errorHandler

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-import Express from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import sequelize from './db.js';
 import models from './models/models.js';
+import router from './routes/index.js';
+import errorHandler from './middleware/ErrorHandlingMiddleware.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const app = new Express();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+app.use(errorHandler);
 
 const runApp = async () => {
   try {

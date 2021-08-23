94ee30ac1c18eeb9864b6aa2efce98f204fb893d
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-08-23 11:23:05 +0300

    create bin dir

--- server/index.js
+++ server/index.js
@@ -1,31 +1,16 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
 
-import sequelize from './db.js';
-import { Patient, Name, Address } from './models/models.js';
 import router from './routes/index.js';
 import errorHandler from './middleware/ErrorHandlingMiddleware.js';
 
-dotenv.config();
+export default async () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use('/api', router);
 
-const PORT = process.env.PORT || 5000;
+  app.use(errorHandler);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/api', router);
-
-app.use(errorHandler);
-
-const runApp = async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
+  return app;
 };
-
-runApp();

e13978f5d805239b713dc1c973b2310362e8e81a
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-08-29 20:20:18 +0300

    change paths for assets

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 import router from './routes/index.js';
 import errorHandler from './middleware/ErrorHandlingMiddleware.js';
@@ -10,10 +8,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = !isProduction;
 const devHost = 'http://localhost:8080';
 const filePath = isDevelopment ? `${devHost}/assets/` : '/dist/public';
-// eslint-disable-next-line no-underscore-dangle
-const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-underscore-dangle
-const __dirname = path.dirname(__filename);
 
 export default () => {
   const app = express();
@@ -21,7 +15,7 @@ export default () => {
   app.use(cors());
   app.use(express.json());
   app.use('/api', router);
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', './server/views');
   app.set('view engine', 'pug');
   app.use(errorHandler);
   app.get('/', (req, res) => {
